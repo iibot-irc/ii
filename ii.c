cf1f7a56b404e56d122002fbfc1c26cf62b44771
nion@localhost <unknown>
2006-11-07 20:21:24 +0100

    compatibility for bitlbee and stuff

--- ii.c
+++ ii.c
@@ -248,7 +248,7 @@ static void proc_channels_input(Channel *c, char *buf)
 		p = strchr(&buf[3], ' ');
 		if(p)
 			*p = 0;
-		if(buf[3]=='#'){
+		if((buf[3]=='#')||(buf[3]=='&')||(buf[3]=='+')||(buf[3]=='!')){
 			snprintf(message, PIPE_BUF, "JOIN %s\r\n", &buf[3]);
 			add_channel(&buf[3]);
 		}
