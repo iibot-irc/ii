c8c4d76195851ff0e14812c9da89bf34d1a8460b
Nico Golde <nion@suckless.org>
2007-04-04 10:35:33 +0200

    ii-1.1

--- config.mk
+++ config.mk
@@ -12,7 +12,7 @@ DESTDIR     =
 
 INCDIR      = ${PREFIX}/include
 LIBDIR      = ${PREFIX}/lib
-VERSION     = 1
+VERSION     = 1.1
 
 # includes and libs
 INCLUDES    = -I. -I${INCDIR} -I/usr/include
