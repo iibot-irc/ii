9a0ea4ada1533bdd7292557cf7087a58bf478810
Nico Golde <nion@suckless.org>
2012-06-16 17:00:51 +0200

    mark 1.7 as the next release version

--- config.mk
+++ config.mk
@@ -12,7 +12,7 @@ DESTDIR     =
 
 INCDIR      = ${PREFIX}/include
 LIBDIR      = ${PREFIX}/lib
-VERSION     = 1.6
+VERSION     = 1.7
 
 # includes and libs
 INCLUDES    = -I. -I${INCDIR} -I/usr/include
